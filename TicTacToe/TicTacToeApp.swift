750e94c41bb9da7a85eaff40acc508b790817e9d
LisandraN08 <lnicoline@student.ciputra.ac.id>
2023-10-20 09:33:57 +0700

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by MacBook Pro on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
